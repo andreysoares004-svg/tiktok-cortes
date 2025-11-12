b24b87e5bea5a1bebadc89d71ac289226ecd123e
andreysoares004-svg <andreysoares004@gmail.com>
2025-11-12 07:36:06 -0300

    Initialize App component with basic structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '2rem' }}>
+      <h1>🚀 TikTok Cortes</h1>
+      <p>Site em construção, em breve com conteúdo dinâmico!</p>
+    </div>
+  );
+}
+
+export default App;
+
